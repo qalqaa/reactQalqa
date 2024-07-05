08bcb5227e012ac277a4e5b4d7440acc3a47c0d2
qalqa <andreybas04@gmail.com>
2024-07-05 13:24:04 +0300

    fix

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -58,15 +58,6 @@ function App() {
         </Button>
         <div>Страница: {page + 1}</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
     </>
   );
 }
